bac60a9ffe81267566f355df66564778cf2c7be4
Benedict John Layos <155413290+Zen0n111@users.noreply.github.com>
2025-07-15 18:27:43 +0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+  const form = document.getElementById('contactForm');
+  const toast = document.getElementById('toast');
+
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    try {
+      await fetch("https://formsubmit.co/YOUR_EMAIL@gmail.com", {
+        method: "POST",
+        body: formData
+      });
+
+      form.reset();
+      toast.classList.add('show');
+      toast.classList.remove('hidden');
+
+      setTimeout(() => {
+        toast.classList.remove('show');
+        toast.classList.add('hidden');
+      }, 4000);
+    } catch (err) {
+      alert('Something went wrong. Please try again.');
+    }
+});
